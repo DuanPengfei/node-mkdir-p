719c39575e2a6783c0d63266f3a9462ba5e8817a
DuanPengfei <2459714173@qq.com>
2017-03-10 17:22:54 +0800

    fix: unhandle error file already exists

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# mkdir-p
+# node-mkdir-p
 
 ### Description
 Linux command `mkdir -p`.
@@ -8,12 +8,12 @@ Linux command `mkdir -p`.
 ```javascript
 const mkdirP = require('mkdir-p');
 
-// default mode
-let result = mkdirP.sync('~/example/app/route');
+// default mode 0o755
+const result = mkdirP.sync('~/example/app/route');
 console.log(result);  // /Users/fei/example/app/route
 
 // specific mode
-let result = mkdirP.sync('~/example/app/route', 755);
+const result = mkdirP.sync('~/example/app/route', 0o755);
 console.log(result);  // /Users/fei/example/app/route
 ```
 
@@ -21,13 +21,13 @@ console.log(result);  // /Users/fei/example/app/route
 ```javascript
 const mkdirP = require('mkdir-p');
 
-// default mode
+// default mode 0o755
 mkdirP.recursion('~/example/app/route', function (error, result) {
     console.log(error, result);  // /Users/fei/example/app/route
 });
 
 // specific mode
-mkdirP.recursion('~/example/app/route', 755, function (error, result) {
+mkdirP.recursion('~/example/app/route', 0o755, function (error, result) {
     console.log(error, result);  // /Users/fei/example/app/route
 })
 ```
--- index.js
+++ index.js
@@ -4,66 +4,67 @@ const fs = require('fs');
 const path = require('path');
 
 /**
- * 
- * @param {String} [_path]
- * @return {String}
+ * fetch absolute path
+ * @param {String} _path relative path
+ * @return {String} _path absolute path
  */
-function absolutePath(_path) {
-    _path = _path || '';
+function _fetchAbsolutePath(_path) {
     _path = path.resolve(_path);
 
-    let index = _path.indexOf('~');
-    if (index !== -1) {
-        _path = process.env.HOME + _path.slice(index + 1);
+    const index = _path.indexOf('~') + 1;
+    if (index) {
+        _path = process.env.HOME + _path.slice(index);
     }
-
+    
     return _path;
-}
+};
 
 /**
- * 
- * @param {String} _path
- * @param {Number} [mode]
- * @param {Function} callback
+ * mkdir -p async
+ * @param {String} _path dir path need to be created
+ * @param {Number} [mode] file permission mode
+ * @param {Function} callback callback function
  */
 function mkdirP(_path, mode, callback) {
     if (typeof mode === 'function') {
         callback = mode;
-        mode = undefined;
+        mode = 0o755;
     }
     callback = callback || function () {};
 
-    _path = absolutePath(_path);
+    _path = _fetchAbsolutePath(_path);
 
     fs.mkdir(_path, mode, function (error) {
         if (error) {
-            if (/no such file or directory/i.test(error.messag)) {
+            if (/no such file or directory/i.test(error.message)) {
                 mkdirP(path.dirname(_path), mode, function (error) {
                     if (error) {
                         return callback(error);
                     } else {
-                        mkdirP(_path, mode,function () {
-                            return callback(undefined, _path);
+                        mkdirP(_path, mode, function () {
+                            return callback(null, _path);
                         });
                     }
                 });
+            } else if (/file already exists/i.test(error.message)) {
+                return callback(null, _path);
             } else {
                 return callback(error);
             }
         } else {
-            return callback(undefined, _path);
+            return callback(null, _path);
         }
     });
 }
 
 /**
- * 
- * @param {String} _path
- * @param {Number} [mode]
- * @return {String}
+ * mkdir -p sync
+ * @param {String} _path dir path need to be created
+ * @param {Number} [mode] file permission mode
+ * @return {String} dir absolute path need to be created
  */
 function mkdirPSync(_path, mode) {
-    _path = absolutePath(_path);
+    _path = _fetchAbsolutePath(_path);
 
     try {
         fs.mkdirSync(_path, mode);
@@ -71,6 +72,8 @@ function mkdirPSync(_path, mode) {
         if (/no such file or directory/i.test(error.message)) {
             mkdirPSync(path.dirname(_path), mode);
             mkdirPSync(_path, mode);
+        } else if (/file already exists/i.test(error.message)) {
+            // do nothing
         } else {
             throw error;
         }
@@ -80,6 +83,6 @@ function mkdirPSync(_path, mode) {
 };
 
 module.exports = {
-    sync: mkdirP,
-    recursion: mkdirPSync
+    recursion: mkdirP,
+    sync: mkdirPSync
 }
